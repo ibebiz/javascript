838a26f1c1842419f9c750df866bad5ad6454b73
nguyentiendat <nguyentiendat03091995@gmail.com>
2016-11-16 09:56:30 +0700

    Khaibaobien

---
+++ JavaScript/Controllers/JavaScriptController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaScript.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        //
+        // GET: /JavaScript/
+        public ActionResult CachKhaiBaoBien()
+        {
+            return View();
+        }
+
+	}
+}
