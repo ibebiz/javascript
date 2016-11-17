c5af4d72ee4da90d6acdefab4542d9d26e24a856
nguyentiendat <nguyentiendat03091995@gmail.com>
2016-11-17 13:13:54 +0700

    Xong T141

---
+++ JavaScript/Content/JJSS.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.Ho = "Nguyễn";
+    $scope.Ten = "Tiến Đạt";
+    $scope.fullName = function () {
+        return $scope.Ho + " " + $scope.Ten;
+    };
+});
--- JavaScript/Controllers/JavaScriptController.cs
+++ JavaScript/Controllers/JavaScriptController.cs
@@ -14,6 +14,10 @@ namespace JavaScript.Controllers
         {
             return View();
         }
+        public ActionResult AJSController()
+        {
+            return View();
+        }
 
 	}
 }
