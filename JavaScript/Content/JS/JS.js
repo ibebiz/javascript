8f68a9868135754b0fbd9d5094dd312adcf333a0
nguyentiendat <nguyentiendat03091995@gmail.com>
2016-11-17 14:44:27 +0700

    Đang kết nối file js

---
+++ JavaScript/Content/JS/JS.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.items = [
+        {
+            id: 1,
+            name: 'iphone7',
+            price: 2500
+        },
+        {
+            id: 2,
+            name: 'Lumia 520',
+            price: 500
+        },
+        {
+            id: 3,
+            name: 'BlackBerry Z10',
+            price: 1500
+        },
+        {
+            id: 4,
+            name: 'SamSung Galaxy S7 Edge',
+            price: 2000
+        }
+    ];
+            
+});
