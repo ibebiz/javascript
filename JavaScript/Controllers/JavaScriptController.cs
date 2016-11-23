869c19e3eeb5b7a4352c036b602f669b81215f94
nguyentiendat <nguyentiendat03091995@gmail.com>
2016-11-23 15:37:25 +0700

    Getdata, XoaDL,

--- JavaScript/Controllers/JavaScriptController.cs
+++ JavaScript/Controllers/JavaScriptController.cs
@@ -12,8 +12,20 @@ namespace JavaScript.Controllers
         // GET: /JavaScript/
         public ActionResult CachKhaiBaoBien()
         {
-            return View();
+
+            var userList = new List<User>();
+            var user = new User();
+            user.Name = "ABC";
+            user.Phone = "0945345";
+            userList.Add(user);
+            var user1 = new User();
+            user1.Name = "EDF";
+            user1.Phone = "0945345";
+
+            userList.Add(user1);
+            return Json(userList, JsonRequestBehavior.AllowGet);
         }
+        
         public ActionResult AngularJSAjax()
         {
             return View();
@@ -28,7 +40,44 @@ namespace JavaScript.Controllers
         //post
         public ActionResult PostDataResponse()
         {
+
             return View();
         }
+
+        public ActionResult Slomotion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+
+            return Json(user);
+        }
+
+        [HttpPost]
+        public ActionResult AddDL(dl user)
+        {
+
+            return Json(user);
+        }
 	}
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+    public class dl
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int PackageTypePriceId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreateBy { get; set; }
+        public int TotalValue { get; set; }
+        public bool IsActive { get; set; }
+    }
 }
---
+++ JavaScript/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JavaScript.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        [ForeignKey("Category")]
+        public Guid CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
+
